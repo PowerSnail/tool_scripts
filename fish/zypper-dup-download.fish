9e30059aa08bd191c59fe431bd97ca6d683b2998
PowerSnail <hj@powersnail.com>
2022-06-17 10:25:24 -0700

    update zypper dup

--- fish/zypper-dup-download.fish
+++ fish/zypper-dup-download.fish
@@ -1,10 +1,12 @@
 #!/usr/bin/fish
 
 function download_until_success
-    while not zypper dup -y -l --no-recommends --download-only 1>/dev/null
+    while not zypper dup -y -l --no-recommends --download-only
         echo "Try again in 10 seconds..."
         sleep 10 & wait $last_pid
     end
 end
 
 download_until_success
+
+echo "Successfully downloaded updates" | mail -s "Successfully downloaded updates" henry
